fa7d283258e8c2b2449abc663029b781b2a48ef3
osama mohamoud <wwwhdosama@gmail.com>
2025-08-30 18:49:10 +0300

    Auto commit: 2025-08-30 18:49:09

--- get min value in vector .cc
+++ get min value in vector .cc
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-short min(vector<int> nums)
+short min(vector<int> &nums)
 {
     short Min = 0;
     for (int n : nums)
@@ -13,9 +13,20 @@ short min(vector<int> nums)
     return Min;
 }
 
+short max(vector<int> &nums)
+{
+    short Max = nums.at(0);
+    for (int n : nums)
+    {
+        if(n>Max) Max = n ;
+    }
+    return Max;
+}
+
 int main()
 {
     vector<int> v{-1, 2, 3, -4, 0, 5, 6, 7};
 
-    cout<<"the min is "<<min(v)<<endl;
+    cout << "the min is " << min(v) << endl;
+     cout << "the max is " << max(v) << endl;
 }
