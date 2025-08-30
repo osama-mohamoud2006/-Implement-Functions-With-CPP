665f0689b335a350c3ddcf568aad3b27ccba2814
osama mohamoud <wwwhdosama@gmail.com>
2025-08-30 17:12:50 +0300

    Auto commit: 2025-08-30 17:12:50

--- string rev.cc
+++ string rev.cc
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 string rev(string str)
 {
     string res = "";
-    for(int i= str.length()-1; i>=0; i--){
-        
-     res+= str[i];
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+
+        res += str[i];
+    }
+    return res;
+}
+string revF(string str)
+{
+    string res;
+    for (int i = str.size() - 1; i >= 0; i--)
+    {
+        if(str[i]==' ') res+=' ';
+       else  res+=str[i];
+
     }
     return res;
 }
 
-int main(){
-    cout<<rev("hi")<<endl;
+
+
+int main()
+{
+    cout << revF("hi world") << endl;
+    ;
 }
