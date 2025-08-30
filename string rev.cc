174ac9233b946451cf2dd9a157fea53f944a4c01
osama mohamoud <wwwhdosama@gmail.com>
2025-08-30 17:29:46 +0300

    Auto commit: 2025-08-30 17:29:46

--- string rev.cc
+++ string rev.cc
@@ -17,17 +17,31 @@ string revF(string str)
     string res;
     for (int i = str.size() - 1; i >= 0; i--)
     {
-        if(str[i]==' ') res+=' ';
-       else  res+=str[i];
 
+        res += str[i];
     }
     return res;
 }
 
+string split(string str, string delmi = " ")
+{
+    short pos = 0;
+    string Fi;
+    string word;
+    while ((pos = str.find(delmi)) != string::npos)
+    {
+        word = str.substr(0,pos);
+        Fi +=revF(word)+delmi;
+        str.erase(0,pos+delmi.length());
 
+    }
+    if(str!="")  Fi +=revF(str);
+
+    return Fi; 
+}
 
 int main()
 {
-    cout << revF("hi world") << endl;
-    ;
+    cout << split("hi world") << endl;
+    
 }
