755176acbc15dcc2c862c59d6b82ca5dfea56de8
osama mohamoud <wwwhdosama@gmail.com>
2025-09-15 13:12:31 +0300

    Auto commit: 2025-09-15 13:12:28

--- substr.cc
+++ substr.cc
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
 
-string SubStr(string str, short start, short length)
+string SubStr(string str, short start, short length , bool inc_end=false,short count_space )
 {
     string res = "";
-    for (int i = start; i < length; i++)
+    for (int i = start; (inc_end ? i<=length : i<length) ; i++)
     {
+        if(str[i])
         res+=str[i];
     }
+ 
     return res ;
 }
 
 int main(){
-    
+    string w = "helloworld!";
+    cout<<"Full string is: "<<w<<endl;
+    cout<<"the sub str from start length 5 "<<SubStr(w,0,5)<<endl;
+        cout<<"the sub str from start length 5 "<<SubStr(w,0,5,true)<<endl;
 }
