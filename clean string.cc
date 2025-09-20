7df7ed4de780d1f793830c40f4c7dcf8085d5f4b
osama mohamoud <wwwhdosama@gmail.com>
2025-09-20 20:02:37 +0300

    Auto commit: 2025-09-20 20:02:35

--- clean string.cc
+++ clean string.cc
@@ -1,20 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-string CleanString(string str){
-    // remove special chars and return only letters,_,nums 
+string CleanString(string str, bool AddNum = false)
+{
+    // remove special chars and return only letters,_,nums
 
     string res = "";
-    
-    for(int i=0; i<res.length(); i++)
+
+    for (int i = 0; i < str.length(); i++)
     {
-        // special char from 33 -> 47
-        if(!(str.at(i)>=33 && str.at(i)>=47) ){
-            res+=str.at(i);
+        if (str.at(i) >= 'A' && str.at(i) <= 'Z')
+            res += str.at(i);
+        else if (str[i] >= 'a' && str[i] <= 'z')
+            res += str.at(i);
+        else if ((str[i] >= '0' && str[i] <= '9') && AddNum == true)
+        {
+            res += str.at(i);
         }
     }
+    return res;
 }
 
-int main(){
-    cout<<"CLEANED STRING: "<<CleaN
+int main()
+{
+    cout << "CLEANED STRING: " << CleanString("o1s#a#m#a",true) << endl;
 }
